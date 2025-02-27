237e01d01bd4e302c781892e61fbc87317dbd85b
OncologyHNJ <bioinformaticaFIBHUNJ@gmail.com>
2025-02-27 15:02:53 +0100

    Update 11_Sigprofilerextractorcnvs.R

--- 11_Sigprofilerextractorcnvs.R
+++ 11_Sigprofilerextractorcnvs.R
@@ -1,5 +1,12 @@
-#######CREATED BY:MARTA PORTASANY########
-#############SIGPROFILEREXTRACTOR########
+# ==================================================
+# Script: 11_Sigprofilerextractorcnvs
+# Description: Execution of SigProfilerExtractorCNVs for single samples of CNV files
+# Author: Marta Portasany
+# Created on: 2025-02-27
+# Last modified: 2025-02-27
+# Pipeline: PULPO
+# Dependencies: reticulate, devtools, SigProfilerMatrixGeneratorR
+# ==================================================
 #########################################
 args <- commandArgs(trailingOnly = TRUE)
 inputdata <- args[1]
@@ -20,24 +27,10 @@ if (!requireNamespace("SigProfilerExtractorR") == TRUE){
   SigProfilerExtractorR::install("GRCh38", rsync=FALSE, bash=TRUE)
   
 }else {
-  message("SigProfilerExtractorR ya está instalado.")
+  message("SigProfilerExtractorR is already installed")
   library("SigProfilerExtractorR")
 }
-#########################################
-#inputdata <- "/home/user/MARTA/PULPO_ejecutadoprueba/results/Patient-83/SigPiler/results/CNVs/Patient-83.CNV48.matrix.tsv"
-#inputdata <- "/home/user/MARTA/PULPO_RevisadoBionano/results/Patients/Patient-1/SigProfiler/results/CNVs/MatrixGenerator/Patient-1.CNV48.matrix.tsv"
-#inputcohort <- "/home/marta/TESIS/OGM+WES/mutationalsignaturesOGM/cohort/cohort.CNV48.matrix.tsv"
-#pythondirectory <- "/home/user/miniconda3/envs/PULPO/bin/python3.10"
-#outputdata <- "/home/user/MARTA/PULPO_RevisadoBionano/results/Patients/Patient-1/SigProfiler/results/CNVs/Extractor/"
-#outputcohort <- "/home/marta/TESIS/OGM+WES/mutationalsignaturesOGM/cohort/results/SigProfiler/CNVs/"
-#filecnv <- read.table(inputcohort,header=TRUE)
-#min_signatures <- "1"
-#max_signatures <- "10"
-#nmf_replicates <- "5"
-#errorlog <- "/home/user/MARTA/PULPO_ejecutadoprueba/logs/Patients/Patient-1/SigProfilerExtractor/Error_log.tsv"
-##ERROR PATIENT 4/18 Error en py_call_impl(callable, call_args$unnamed, call_args$named): ValueError: Length of values (1) does not match length of index (3)
-##ERROR PATIENT 6 -Error en py_call_impl(callable, call_args$unnamed, call_args$named): ValueError: operands could not be broadcast together with shapes (48,0) (0,0) 
-#########################################
+
 use_python(pythondirectory)
 py_config()
 #########################################
@@ -52,5 +45,4 @@ if (file.exists(inputdata) == FALSE) {
   sigprofilerextractor("matrix", outputdata, inputdata, reference_genome = "GRCh38", minimum_signatures=min_signatures , maximum_signatures=max_signatures, nmf_replicates = nmf_replicates, min_nmf_iterations = 1000, max_nmf_iterations =100000, nmf_test_conv = 1000, nmf_tolerance = 0.00000001)
 }
 
-#sigprofilerextractor("matrix", "/home/user/MARTA/PULPO_RevisadoBionano/results/Cohort/prueba/", inputdata, reference_genome = "GRCh38", minimum_signatures=1 , maximum_signatures=10, nmf_replicates = 5, min_nmf_iterations = 1000, max_nmf_iterations =100000, nmf_test_conv = 1000, nmf_tolerance = 0.00000001)
 
